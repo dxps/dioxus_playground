2b2d0300b03a29657a79f89e507f10506e4e531e
dxps <marius.ileana@gmail.com>
2024-04-19 20:50:16 +0300

    dioxus_axum_auth_pgdb: add TailwindCSS

--- dioxus_axum_auth_pgdb/src/ui/pages/app.rs
+++ dioxus_axum_auth_pgdb/src/ui/pages/app.rs
@@ -7,43 +7,55 @@ pub fn app() -> Element {
 
     rsx! {
         div {
-            button { onclick: move |_| {
-                    async move {
-                        login().await.unwrap();
+            class: "bg-gray-100",
+            div { class: "flex flex-row min-h-screen justify-center items-center drop-shadow-2xl",
+                table { class: "bg-white rounded-md m-2",
+                    tr {
+                        td { class: "m-2 p-2",
+                            button {
+                                class: "bg-green-50 drop-shadow-sm px-2 py-1 rounded-md",
+                                onclick: move |_| { async move { login().await.unwrap(); } },
+                                "Login Test User"
+                            }
+                        }
+                        td {}
                     }
-                },
-                "Login Test User"
-            }
-        }
-        div {
-            button {
-                onclick: move |_| async move {
-                    if let Ok(data) = get_user_name().await {
-                        user_name.set(data);
+                    tr {
+                        td { class: "m-2 p-2",
+                            button {
+                                class: "bg-blue-50 px-2 py-1 rounded-md",
+                                onclick: move |_| async move {
+                                    if let Ok(data) = get_user_name().await { user_name.set(data); }
+                                },
+                                "Get User Name"
+                            }
+                        }
+                        td { "User name: {user_name}" }
                     }
-                },
-                "Get User Name"
-            }
-            "User name: {user_name}"
-        }
-        div {
-            button {
-                onclick: move |_| async move {
-                    if let Ok(data) = get_permissions().await {
-                        permissions.set(data);
+                    tr {
+                        td { class: "m-2 p-2",
+                            button {
+                                class: "bg-blue-50 px-2 py-1 rounded-md",
+                                onclick: move |_| async move {
+                                    if let Ok(data) = get_permissions().await {
+                                        permissions.set(data);
+                                    }
+                                },
+                                "Get Permissions"
+                            }
+                        }
+                        td { "Permissions: {permissions}" }
                     }
-                },
-                "Get Permissions"
-            }
-            "Permissions: {permissions}"
-        }
-        div {
-            button { onclick: move |_| {
-                    async move {
-                        logout().await.unwrap();
+                    tr {
+                        td { class: "m-2 p-2", colspan: "2",
+                            button {
+                                class: "bg-gray-100 px-2 py-1 rounded-md",
+                                onclick: move |_| { async move { logout().await.unwrap(); } },
+                                "Logout Test User"
+                            }
+                        }
                     }
-                },
-                "Logout Test User"
+                }
             }
         }
     }
