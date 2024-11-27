41e6daef4269af06c7f2fdc0dcba308b88792f4d
dxps <marius.ileana@gmail.com>
2024-11-27 21:42:46 +0200

    dioxus_05_list-dnd: 1st working example w/ static list

---
+++ dioxus_05_list-dnd/readme.md
@@ -0,0 +1,13 @@
+# Dioxus 0.5 :: HTML List Drag-n-Drop
+
+...
+
+<br/>
+
+## Usage
+
+```bash
+dx serve --hot-reload
+```
+
+-   Open the browser to http://localhost:8080
---
+++ dioxus_05_list-dnd/src/main.rs
@@ -0,0 +1,106 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        div {
+            div { class: "flex flex-col min-h-screen bg-gray-100",
+                div { class: "flex flex-col min-h-screen justify-center items-center drop-shadow-2xl",
+                    div { class: "bg-white rounded-md p-6 mt-8 mb-8 w-[600px]",
+                        h2 { class: "text-3xl text-gray-500 font-medium text-center",
+                            "HTML List Drag-n-Drop"
+                        }
+                        hr { class: "mb-8" }
+                        SortableList {}
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[component]
+fn SortableList() -> Element {
+    //
+    let mut drag_source_item = use_signal(|| 0);
+    let mut drag_target_item = use_signal(|| 0);
+    rsx! {
+        div { class: "mb-4 flex flex-col items-center",
+            div { class: "text-gray-500", "Drag and drop items to reorder: " }
+            div { class: "text-gray-700", " {drag_source_item} -> {drag_target_item}" }
+        }
+        ul { class: "list-disc ml-4",
+            li {
+                draggable: true,
+                ondragstart: move |e| {
+                    e.stop_propagation();
+                    drag_source_item.set(1);
+                },
+                ondragover: move |e| {
+                    e.stop_propagation();
+                    drag_target_item.set(1);
+                },
+                "Item 1"
+            }
+            li {
+                draggable: true,
+                ondragstart: move |e| {
+                    e.stop_propagation();
+                    drag_source_item.set(2);
+                },
+                ondragover: move |e| {
+                    e.stop_propagation();
+                    drag_target_item.set(2);
+                },
+                "Item 2"
+            }
+            li {
+                draggable: true,
+                ondragstart: move |e| {
+                    e.stop_propagation();
+                    drag_source_item.set(3);
+                },
+                ondragover: move |e| {
+                    e.stop_propagation();
+                    drag_target_item.set(3);
+                },
+                "Item 3"
+            }
+            li {
+                draggable: true,
+                ondragstart: move |e| {
+                    e.stop_propagation();
+                    drag_source_item.set(4);
+                },
+                ondragover: move |e| {
+                    e.stop_propagation();
+                    drag_target_item.set(4);
+                },
+                "Item 4"
+            }
+        }
+    }
+}
