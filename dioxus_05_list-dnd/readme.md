22fbe465df2e989d3dd415035dff143d19a5fbd4
dxps <marius.ileana@gmail.com>
2024-11-29 10:48:48 +0200

    dioxus_05_list-dnd: react on changes (wip)

--- dioxus_05_list-dnd/readme.md
+++ dioxus_05_list-dnd/readme.md
@@ -1,13 +1,14 @@
 # Dioxus 0.5 :: HTML List Drag-n-Drop
 
-...
+A minimal drag and drop example for arranging the elements in an HTML list.
 
 <br/>
 
 ## Usage
 
-```bash
-dx serve --hot-reload
-```
+Run both:
 
--   Open the browser to http://localhost:8080
+-   `./run_dev.sh` - to start it in "development" mode
+-   `./run_css.sh` - if you change styling (mainly CSS) related elements
+
+Go to http://localhost:3003 and play with the result.
--- dioxus_05_list-dnd/src/sortable_list.rs
+++ dioxus_05_list-dnd/src/sortable_list.rs
@@ -1,11 +1,12 @@
 use dioxus::prelude::*;
+use dioxus_elements::source;
 use dioxus_logger::tracing::info;
 
 #[component]
 pub fn SortableList(items: Signal<Vec<(u32, String)>>) -> Element {
     //
-    let mut drag_source_item = use_signal(|| 0u32);
-    let mut drag_target_item = use_signal(|| 0u32);
+    let mut drag_source_index = use_signal(|| 0u32);
+    let mut drag_target_index = use_signal(|| 0u32);
 
     // Have its own copy to prevent an infinite loop (that would happen
     // when reading and writing to the same signal in the same scope)
@@ -13,36 +14,49 @@ pub fn SortableList(items: Signal<Vec<(u32, String)>>) -> Element {
     let mut my_items = use_signal(|| Vec::from(items()));
 
     use_effect(move || {
-        let source_item = drag_source_item();
-        let target_item = drag_target_item();
-        if source_item != target_item && source_item > 0 && target_item > 0 {
+        let source_index = drag_source_index();
+        let target_index = drag_target_index();
+        if source_index != target_index && source_index > 0 && target_index > 0 {
             info!(
-                ">>> React on changes: drag_source_item: {} drag_target_item: {}",
-                drag_source_item(),
-                drag_target_item()
+                ">>> React on changes: source_index: {} target_index: {}",
+                source_index, target_index
             );
             my_items.set(items());
         }
     });
 
     use_effect(move || {
-        let source_item = drag_source_item();
-        let target_item = drag_target_item();
-        if source_item != target_item && source_item > 0 && target_item > 0 {
+        let source_index = usize::try_from(drag_source_index()).unwrap();
+        let target_index = usize::try_from(drag_target_index()).unwrap();
+        if source_index != target_index {
+            let mut new_items = items();
+
+            if source_index < target_index {
+                // Dragging down.
+                let source_item = new_items.get(source_index).unwrap().clone();
+                new_items.remove(source_index);
+                if target_index < new_items.len() {
+                    new_items.insert(target_index, source_item);
+                } else {
+                    new_items.push(source_item);
+                }
+            } else {
+                // Dragging up.
+            }
             // Swap items.
-            let mut reordered_items = items();
-            reordered_items.swap(
-                usize::try_from(drag_source_item() - 1).unwrap(),
-                usize::try_from(drag_target_item() - 1).unwrap(),
-            );
-            my_items.set(reordered_items);
+            // reordered_items.swap(
+            //     usize::try_from(drag_source_item() - 1).unwrap(),
+            //     usize::try_from(drag_target_item() - 1).unwrap(),
+            // );
+            info!(">>> React on changes: newitems: {:?} ", new_items);
+            my_items.set(new_items);
         }
     });
 
     rsx! {
         div { class: "mb-4 flex flex-col items-center",
             div { class: "text-gray-500", "Drag and drop items to reorder: " }
-            div { class: "text-gray-700", " {drag_source_item} -> {drag_target_item}" }
+            div { class: "text-gray-700", " {drag_source_index} -> {drag_target_index}" }
         }
         ul { class: "list-disc ml-4",
             for item in my_items() {
@@ -50,11 +64,11 @@ pub fn SortableList(items: Signal<Vec<(u32, String)>>) -> Element {
                     draggable: true,
                     ondragstart: move |e| {
                         e.stop_propagation();
-                        drag_source_item.set(item.0);
+                        drag_source_index.set(item.0 - 1);
                     },
                     ondragover: move |e| {
                         e.stop_propagation();
-                        drag_target_item.set(item.0);
+                        drag_target_index.set(item.0 - 1);
                     },
                     "{item.1}"
                 }
