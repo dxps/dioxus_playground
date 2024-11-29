7c928cdbf09516b4e7fe1c8d19c95329e12163e1
dxps <marius.ileana@gmail.com>
2024-11-29 23:35:41 +0200

    dioxus_05_list-dnd: almost done (wip)

--- dioxus_05_list-dnd/src/main.rs
+++ dioxus_05_list-dnd/src/main.rs
@@ -1,10 +1,11 @@
 #![allow(non_snake_case)]
 
-mod sortable_list;
+mod dnd_list;
 
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
-use sortable_list::SortableList;
+use dnd_list::DnDList;
+use indexmap::IndexMap;
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 enum Route {
@@ -13,10 +14,7 @@ enum Route {
 }
 
 fn main() {
-    // Init logger
-    dioxus_logger::init(Level::INFO).expect("failed to init logger");
-    info!("starting app");
-
+    dioxus_logger::init(Level::INFO).expect("Failed to init logger!");
     launch(App);
 }
 
@@ -29,13 +27,32 @@ fn App() -> Element {
 #[component]
 fn Home() -> Element {
     //
-    let items = use_signal(|| {
-        vec![
-            (1, "Item 1".to_string()),
-            (2, "Item 2".to_string()),
-            (3, "Item 3".to_string()),
-            (4, "Item 4".to_string()),
-        ]
+    let mut items = IndexMap::from([
+        ("0".to_string(), "First Item".to_string()),
+        ("1".to_string(), "Second Item".to_string()),
+        ("2".to_string(), "Third Item".to_string()),
+        ("3".to_string(), "Fourth Item".to_string()),
+    ]);
+
+    let mut new_items = use_signal(|| IndexMap::<String, String>::from(items.clone()));
+    let order_change = use_signal(|| (0, 0));
+
+    use_effect(move || {
+        let (source_index, target_index) = order_change();
+        if source_index != target_index {
+            let mut changed_items = items.clone();
+            if source_index < target_index {
+                for index in source_index..target_index {
+                    info!(">>> [Home] Swapping {} and {}", index, index + 1);
+                    changed_items.swap_indices(index, index + 1);
+                }
+                info!(">>> [Home] After swap, changed_items: {:?}", changed_items);
+                items = changed_items.clone();
+                new_items.set(changed_items);
+            } else {
+                // TODO
+            }
+        }
     });
 
     rsx! {
@@ -44,10 +61,10 @@ fn Home() -> Element {
                 div { class: "flex flex-col min-h-screen justify-center items-center drop-shadow-2xl",
                     div { class: "bg-white rounded-md p-6 mt-8 mb-8 w-[600px]",
                         h2 { class: "text-3xl text-gray-500 font-medium text-center",
-                            "HTML List Drag-n-Drop"
+                            "Drag-n-Drop List"
                         }
                         hr { class: "mb-8" }
-                        SortableList { items }
+                        DnDList { items: new_items, order_change }
                     }
                 }
             }
