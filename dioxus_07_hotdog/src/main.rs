77d3380ebc4fe2f084ca0473ad90628c8c67b10f
dxps <marius.ileana@gmail.com>
2025-12-01 20:50:44 +0200

    dioxus_07_hotdog: use_resource to fetch images

--- dioxus_07_hotdog/src/main.rs
+++ dioxus_07_hotdog/src/main.rs
@@ -4,6 +4,11 @@ const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
 fn main() {
     dioxus::launch(App);
 }
@@ -14,18 +19,49 @@ fn App() -> Element {
         document::Link { rel: "icon", href: FAVICON }
         document::Stylesheet { href: MAIN_CSS }
         document::Stylesheet { href: TAILWIND_CSS }
+        Title {}
+        DogView {}
+    }
+}
+
+#[component]
+fn Title() -> Element {
+    rsx! {
         div { id: "title",
             h1 { "HotDog! 🌭" }
         }
+    }
+}
+
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
+    rsx! {
         div { id: "dogview",
-            img {
-                src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg",
-                max_height: "300px",
-            }
+            img { src: img_src, max_height: "300px" }
         }
         div { id: "buttons", class: "flex flex-row gap-2",
-            button { id: "skip", class: "cursor-pointer", "skip" }
-            button { id: "save", class: "cursor-pointer", "save!" }
+            button {
+                id: "skip",
+                onclick: move |_| img_src.restart(),
+                class: "cursor-pointer",
+                "skip"
+            }
+            button {
+                id: "save",
+                onclick: move |_| img_src.restart(),
+                class: "cursor-pointer",
+                "save!"
+            }
         }
     }
 }
