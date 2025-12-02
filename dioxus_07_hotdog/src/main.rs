26973f6da47ffb5eb7fc9a42f6ed77205fd21f35
dxps <marius.ileana@gmail.com>
2025-12-02 17:26:09 +0200

    feat(dioxus_07_hotdog): using sqlite in backend
    Instead of a file.

--- dioxus_07_hotdog/src/main.rs
+++ dioxus_07_hotdog/src/main.rs
@@ -82,20 +82,33 @@ fn DogView() -> Element {
     }
 }
 
+///////////////////////
+/// the server side ///
+///////////////////////
+
+#[cfg(feature = "server")]
+thread_local! {
+    pub static DB: rusqlite::Connection = {
+        // Open the database from the persisted "hotdog.db" file
+        let conn = rusqlite::Connection::open("hotdog.db").expect("Failed to open database");
+
+        // Create the "dogs" table if it doesn't already exist
+        conn.execute_batch(
+            "CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                url TEXT NOT NULL
+            );",
+        ).unwrap();
+
+        // Return the connection
+        conn
+    };
+}
+
+#[server]
 #[post("/api/save_dog")]
 async fn api_save_dog(image: String) -> Result<()> {
-    use std::io::Write;
-
-    // Open the `dogs.txt` file in append-only mode, creating it if it doesn't exist.
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open("dogs.txt")
-        .unwrap();
-
-    // And then write a newline to it with the image url.
-    file.write_fmt(format_args!("{image}\n"));
+    DB.with(|f| f.execute("INSERT INTO dogs (url) VALUES (?)", &[&image]))?;
 
     Ok(())
 }
